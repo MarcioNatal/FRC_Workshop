93f15bcc97244014f9e8a97679d5ed8c127532d5
MarcioNatal <mn_natal@yahoo.com.br>
2024-11-08 10:02:34 -0300

    Update RobotContainer.java

--- lab 6/src/main/java/frc/robot/RobotContainer.java
+++ lab 6/src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,12 @@ package frc.robot;
 
 
 //WPI Imports
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 //LOCAL Imports
